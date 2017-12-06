917daf35780ad5f093bec545d3c4db8e2a8b6b07
Tyrone Scafe <30609097+TScafeJR@users.noreply.github.com>
2017-12-05 16:12:21 -0800

    update postinstall script so that it doesn't run everytime we fire up the database

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-
 /*
 My passport strategy is below 
 */
